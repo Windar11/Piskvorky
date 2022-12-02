afcb0fd1b90cd736262d169e42b2faff47037b29
Peter <petercarny11@gmail.com>
2022-12-02 14:46:00 +0100

    epic cool comit

---
+++ Piskvorky/Piskvorky.cpp
@@ -0,0 +1,12 @@
+﻿// Piskvorky.cpp : Defines the entry point for the application.
+//
+
+#include "Piskvorky.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world." << endl;
+	return 0;
+}
